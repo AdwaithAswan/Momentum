89f39c6d5c355b68ccb0bfca075577d070e638bc
Adwaith Aswan <adwaithaswakumar@gmail.com>
2026-03-24 10:54:00 +0530

    2. update in port

--- backend/run.py
+++ backend/run.py
@@ -34,4 +34,4 @@ def open_browser():
 if __name__ == "__main__":
     print("✅ Opening http://localhost:5000 in your browser...")
     threading.Thread(target=open_browser, daemon=True).start()
-    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=10000, debug=False, threaded=True)
